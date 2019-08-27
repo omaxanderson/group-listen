f1f92f31397c2216a242446085ce5aef94e876d8
O Max Anderson <omaxwellanderson@gmail.com>
2019-08-26 22:06:29 -0500

    get login functionality working

--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,101 @@
 import "reflect-metadata";
 import fastify from 'fastify';
+import fastifySession from 'fastify-session';
+import fastifyCookie from 'fastify-cookie';
 import { IncomingMessage, Server, ServerResponse } from 'http';
 import { getMongoManager, Connection, createConnection } from 'typeorm';
 import Queue from './entity/Queue';
+import { promises } from 'fs';
+import path from 'path';
+import Api from './spotify_api/api';
 
-const server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({});
+(async () => {
+   const server: fastify.FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({});
 
-const port: number = Number(process.env.PORT || 3000);
-let connection: Connection;
+   const port: number = Number(process.env.PORT || 3000);
+   const domain: string = process.env.ENVIRONMENT === 'production' ? 'something' : 'localhost';
+   let connection: Connection;
+   let client_id;
+   let client_secret;
 
-server.get('/ping', (req, res) => {
-   res.send({ message: 'max' });
-});
-
-server.get('/test', async (req, res) => {
-   const queue = new Queue();
-   queue.songs = ['song1', 'song2', 'Naeem'];
-   const manager = getMongoManager();
    try {
-      const result = await manager.save(queue);
-      res.send(result);
+      client_id = await promises.readFile(path.join(__dirname, '../client_id.txt'), 'utf8');
+      client_secret = await promises.readFile(path.join(__dirname, '../client_secret.txt'), 'utf8');
+      client_id = client_id.replace(/\s/g, '');
+      client_secret = client_secret.replace(/\s/g, '');
    } catch (e) {
       console.log(e);
+      process.exit(1);
    }
-});
 
-const start = () => {
-   server.listen(port, '0.0.0.0', (err, address) => {
-      if (err) {
-         console.log('err:', err);
-         process.exit(1);
+   const api = new Api({ client_id, client_secret });
+
+   server.register(fastifyCookie);
+   server.register(fastifySession, {
+      secret: 'e59a7b781745ac15455de026297f024742209f17',
+      cookie: {
+         secure: false,
+         maxAge: 1000 * 60 * 60 * 6,
+      },
+      saveUninitialized: true,
+   });
+
+   server.get('/ping', (req, res) => {
+      res.send({ message: 'max' });
+   });
+
+   server.get('/test', async (req, res) => {
+      const queue = new Queue();
+      queue.songs = ['song1', 'song2', 'Naeem'];
+      const manager = getMongoManager();
+      try {
+         const result = await manager.save(queue);
+         return result;
+      } catch (e) {
+         console.log(e);
+         return e;
       }
-
-      console.log(`listening on port ${address}`);
    });
-}
 
-const setup = async () => {
-   connection = await createConnection({
-      "type": "mongodb",
-      "host": "localhost",
-      "port": 27017,
-      "database": "group_listen",
-      "useNewUrlParser": true,
-      "entities": [ Queue ],
+   server.get('/login', (req, res) => {
+      api.authorize(req, res, {
+         redirect_uri: `http://${domain}:${port}/authorize`,
+         scopes: ['playlist-read-private'],
+      });
    });
-}
 
-setup().then(start);
+   // Should only be called as the redirect uri from spotify login
+   server.get('/authorize', async (req, res) => {
+      const { code, state } = req.query;
+      return await api.login(req, res, {
+         client_id,
+         client_secret,
+         code,
+         redirect_uri: `http://${domain}:${port}/authorize`,
+      });
+   });
+
+   const start = () => {
+      server.listen(port, '0.0.0.0', (err, address) => {
+         if (err) {
+            console.log('err:', err);
+            process.exit(1);
+         }
+
+         console.log(`listening on port ${address}`);
+      });
+   }
+
+   const setup = async () => {
+      connection = await createConnection({
+         "type": "mongodb",
+         "host": "localhost",
+         "port": 27017,
+         "database": "group_listen",
+         "useNewUrlParser": true,
+         "entities": [ Queue ],
+      });
+   }
+
+   setup().then(start);
+})();
